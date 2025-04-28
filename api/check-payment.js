e3ad5f6e7cb6124e18fe709d63df99faa6dbcf48
C. Pekcan Birinci <pekcan@Cs-MacBook-Air.local>
2025-04-28 20:15:59 +0300

    API dosyaları düzeltildi

--- api/check-payment.js
+++ api/check-payment.js
@@ -1,22 +1,11 @@
 export default function handler(req, res) {
     const { username } = req.query;
   
-    let email = '';
-    if (username === 'tellakebap.1') email = 'pekcan@example.com';
-    else if (username === 'admin') email = 'admin@example.com';
-    else if (username === 'demo') email = 'demo@example.com';
-    else email = `${username}@example.com`;
-  
-    // Şu anda fake kontrol yapıyoruz. Gerçek kontrol database'den yapılacak.
-    const paymentInfo = JSON.parse(localStorage.getItem('paymentInfo') || '{}');
-    const now = new Date();
-    const paidDate = new Date(paymentInfo.date);
-    const diffInDays = (now - paidDate) / (1000 * 60 * 60 * 24);
-  
-    if (diffInDays <= 30) {
+    // Şu anda fake sabit değer dönüyoruz. (Gerçekte database sorgulanır)
+    if (username) {
       res.status(200).json({ paid: true });
     } else {
-      res.status(200).json({ paid: false });
+      res.status(400).json({ paid: false });
     }
   }
   
